a910c066b237741bb023b8edb4a250d42d8d68c9
sabya1992 <44781116+sabya1992@users.noreply.github.com>
2018-12-26 01:56:30 -0600

    update card description

--- service/src/main/resources/dao/card.sql
+++ service/src/main/resources/dao/card.sql
@@ -5,13 +5,15 @@ INSERT INTO card (
   card_number,
   card_image,
   card_image_mime_type,
-  card_type, card_description 
+  card_type, 
+  card_description 
 ) VALUES(
   :card_name,
   :card_number,
   :card_image,
   :card_image_mime_type,
-  :card_type, :card_description 
+  :card_type, 
+  :card_description 
 )
 
 
@@ -19,7 +21,9 @@ INSERT INTO card (
 
 SELECT * FROM card WHERE id = :id
 --STATEMENT updateCard
+
 UPDATE card SET card_name = :card_name, card_number = :card_number,card_image= :card_image,card_image_mime_type= :card_image_mime_type,card_type = :card_type, card_description = :card_description WHERE id = :id
+
 --STATEMENT deleteCard
 
 DELETE FROM card WHERE id = :id
